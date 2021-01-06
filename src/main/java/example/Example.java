d738fd58d3db6d348e9070cfbc714d8363844c8f
HeinrichWizardKreuser <hwkreuser@gmail.com>
2021-01-06 20:07:14 +0200

    added example of how to use messageListeners and setTitle(), cleaned imports, added javadocs

--- src/main/java/example/Example.java
+++ src/main/java/example/Example.java
@@ -3,29 +3,13 @@ package example;
 import com.github.heinrichwizardkreuser.javafxchatbubbles.*;
 
 import javafx.application.Application;
-import javafx.event.EventHandler;
-import javafx.geometry.Pos;
-import javafx.geometry.Insets;
-
-import javafx.scene.control.*;
-import javafx.scene.Group;
-import javafx.beans.value.*;
-import javafx.scene.layout.*;
+import javafx.scene.control.Label;
+import javafx.scene.control.SplitPane;
 import javafx.scene.Scene;
-import javafx.scene.text.TextAlignment;
-import javafx.scene.paint.Color;
-
 import javafx.stage.Stage;
-import javafx.stage.Modality;
-import javafx.collections.*;
-import javafx.concurrent.Task;
-import javafx.application.*;
-import javafx.stage.WindowEvent;
-
 
 public class Example extends Application {
 
-  /** launches the GUI */
   public static void main(String[] args) {
     launch(args);
   }
@@ -33,23 +17,30 @@ public class Example extends Application {
   @Override
   public void start(Stage primaryStage) throws Exception {
 
-    Chat exampleChat = new Chat("us");
-    exampleChat.add(new Message("Friend 1", "Hi there!"));
-    exampleChat.add(new Message("us", "what is up friend 1!"));
+    // let's setup an example of how a chat would look like
+    Chat exampleChat = new Chat("Finn"); // we pass our username as an argument
+    exampleChat.add(new Message("Jake", "Hi there!"));
+    exampleChat.add(new Message("Finn", "What up, Jake!"));
     ChatPane chatPane = new ChatPane(exampleChat);
 
-    // main pane 
+    // we can set the title of the ChatPane like so
+    chatPane.setTitle("Chat with Jake");
+
+    /*next, we will put the chat pan into a random splitpane to showcase how
+      it can be used alongside other panes*/
     SplitPane splitPane = new SplitPane();
     splitPane.getItems().addAll(new Label("something here"), 
       chatPane);
 
-    // TextField textfield = chatPane.getTextField();
-    // textfield.setOnAction(e -> {
-    //   String msg = textfield.getText();
-    //   System.out.println("message:" + msg);
-    //   textfield.clear();
-    // });
-
+    /*we can also add listeners to new messages entered by the user if the 
+      developer would like to add more actions to when messages are typed in.
+      For example, we can make that the message typed by the user be sent 
+      across a network from here.*/
+    chatPane.setMessageListener((msg, chat) -> {
+      System.out.printf("new Message '%s' from '%s' in chat '%s'\n", 
+        msg.content, msg.sender, chat.toString());
+    });
+  
     primaryStage.setScene(new Scene(splitPane));
     primaryStage.show();
   }
